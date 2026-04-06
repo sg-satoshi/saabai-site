a6262909a30792c26d15a12da42447441cac4942
sg-satoshi <shanegoldberg@gmail.com>
2026-04-06 19:28:30 +1000

    fix: lex-widget force-dynamic to prevent static 404

--- app/lex-widget/page.tsx
+++ app/lex-widget/page.tsx
@@ -1,9 +1,9 @@
-"use client";
-
 import LexWidget from "../components/LexWidget";
 
 // Embeddable iframe page for Tributum Law website
 // Embed: <iframe src="https://saabai.ai/lex-widget" style="position:fixed;bottom:0;right:0;width:420px;height:680px;border:none;z-index:9999;" />
+
+export const dynamic = "force-dynamic";
 
 export default function LexWidgetPage() {
   return (
