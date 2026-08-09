bf719d3661cad0d021543f79a7f2dabb618f15bd
Shane Goldberg <shanegoldberg@pm.me>
2026-08-09 21:09:12 +1000

    chore: flush changelog entries for websites launch

--- app/saabai-changelog/SaabaiChangelogClient.tsx
+++ app/saabai-changelog/SaabaiChangelogClient.tsx
@@ -30,6 +30,7 @@ const CHANGELOG: Day[] = [
   {
     date: "9 Aug 2026",
     entries: [
+      { time: "20:50", tag: "NEW", title: "Link Websites offering in nav and footer" },
       { time: "20:50", tag: "NEW", title: "Add AI-native websites offering page" },
       { time: "20:15", tag: "UPDATE", title: "Web services offering implementation plan" },
       { time: "20:00", tag: "UPDATE", title: "Web services offering design spec" }
